90cc8082bae549963688071ba7f5757ff8be2f81
Jochem Vogel <voge0031@hz.nl>
2021-03-09 17:24:47 +0100

    Add working user input
    Does nothing besides a console log, but it does at least something

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 // const fetch = require("node-fetch");
 const app = express();
 const port = 3000;
 
+const middlewares = [bodyParser.urlencoded({ extended: true })];
+
+app.use(middlewares);
 app.use(express.static("static"));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        title: "Home",
-    });
+    res.render("home");
+});
+
+app.post("/", (req, res) => {
+    res.render("home");
+    const fromLocation = req.body.from;
+    const toLocation = req.body.to;
+    const date = req.body.date;
+    console.log(`From ${fromLocation} to ${toLocation} on ${date}`);
 });
 
 app.get("/checkout", (req, res) => {
-    res.render("checkout", {
-        title: "Checkout",
-    });
+    res.render("checkout");
 });
 
 app.get("*", (req, res) => {
-    res.render("404", {
-        title: "Not found",
-    });
+    res.render("404");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
