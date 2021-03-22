d1fb99ec5e112d28f4514d58937085350d86e4a8
Jochem Vogel <voge0031@hz.nl>
2021-03-22 15:12:35 +0100

    Add revRewrite

--- scripts/build-rev.js
+++ scripts/build-rev.js
@@ -1,5 +1,6 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
 const rev = require('gulp-rev');
+const revRewrite = require('gulp-rev-rewrite');
 
 return gulp.src([
         'public/css/*.css', 'public/js/*.js',
@@ -8,4 +9,5 @@ return gulp.src([
     .pipe(rev())
     .pipe(gulp.dest('public'))
     .pipe(rev.manifest())
+    .pipe(revRewrite())
     .pipe(gulp.dest('public'))
