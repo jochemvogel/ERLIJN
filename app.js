42ec1bd3f8f9dc32e3e5aa417d181e556fab5ec1
Jochem Vogel <voge0031@hz.nl>
2021-03-22 13:16:14 +0100

    Remove htaccess and add redirect to app.js
    Redirect http to https

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ const middlewares = [
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(function (req, res, next) {
+    if (req.secure ||req.headers.host.includes('localhost') ) {
+        // request was via https, so do no special handling
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect("https://" + req.headers.host + req.url);
+    }
+});
+
 app.use(middlewares);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
