ef024e81e3eb77c22915532d80ebea9d4d1050f9
Jochem Vogel <voge0031@hz.nl>
2021-03-29 09:37:02 +0200

    Update cache assets sw

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -2,7 +2,6 @@ const CORE_CACHE = 3;
 const CORE_CACHE_NAME = `core-v${CORE_CACHE}`;
 const CORE_ASSETS = [
     "manifest.json",
-    "/img/icons",
     "css/bundle.min.css",
     "js/bundle.min.js",
     "/",
